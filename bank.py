4cee2ac71f8084ef94af130cf4c837c08aacdfbf
Ilya Karpovs <karpov.ilja@gmail.com>
2024-01-17 08:44:09 +0200

    Initial blank app

---
+++ bank.py
@@ -0,0 +1,32 @@
+# 
+# Darbības ar masīviem - https://www.w3schools.com/python/python_arrays.asp
+# Datu tipi, mekle float - https://www.w3schools.com/python/python_datatypes.asp
+# Pārbaudēm izmanto if...else - https://www.w3schools.com/python/python_conditions.asp
+# Cipari https://www.w3schools.com/python/python_numbers.asp
+# 
+
+balance = 0
+
+while True:
+    print("\nBanking Options:")
+    print("1. Deposit")
+    print("2. Withdraw")
+    print("3. Check Balance")
+    print("4. Exit")
+
+    choice = input("Enter your choice (1-4): ")
+
+    if choice == '1':
+        # papildini kodu šeit
+        pass
+    elif choice == '2':
+        # papildini kodu šeit
+        pass
+    elif choice == '3':
+        # papildini kodu šeit
+        pass
+    elif choice == '4':
+        print("Exiting the banking system. Thank you!")
+        break
+    else:
+        print("Invalid choice. Please enter a number between 1 and 5.")
